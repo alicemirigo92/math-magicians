65e823206cf09e96e100f6b51e43a513ed0e5d3b
alicemirigo92 <alicemirigo92@gmai.com>
2023-03-01 12:53:09 +0300

    Add website navigations

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import style from './Header.module.css';
+
+const Header = () => (
+  <header className={style.header}>
+    <h1 className={style.h1}>Math Magicians</h1>
+    <nav>
+      <ul className={style.nav}>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Home</NavLink>
+        </li>
+        |
+        <li>
+          <NavLink to="/calculator" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Calculator</NavLink>
+        </li>
+        |
+        <li>
+          <NavLink to="/quote" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Quote</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
