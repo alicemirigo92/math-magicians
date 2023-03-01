fac940b9abefd227400ebf5c8876a915ccce6002
alicemirigo92 <alicemirigo92@gmai.com>
2023-03-01 18:08:00 +0300

    Add snapshot to components

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+reportWebVitals();
